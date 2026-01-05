0344f9a83420d6d666fca051b6d1c46eaf2b95b8
AmitOfeckMoveo <amit.ofeck@moveo.co.il>
2026-01-05 16:22:55 +0200

    chore: setup eslint, prettier and tailwind linting
    # Lint code
    npm run lint
    
    # Format code
    npm run format
    
    # Check formatting without changing files
    npm run format:check

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,15 +5,25 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:tailwindcss/recommended',
+    'plugin:prettier/recommended', // Must be last
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: ['react-refresh', 'tailwindcss'],
+  settings: {
+    tailwindcss: {
+      callees: ['cn', 'cva'],
+      config: 'tailwind.config.js',
+    },
+  },
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'prettier/prettier': 'warn', // Prefer warnings over errors
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
   },
-}
-
+};
