24de847ac912981ea1513a9de5fbdfcb034f9c45
AmitOfeckMoveo <amit.ofeck@moveo.co.il>
2026-01-06 10:23:37 +0200

    refactor(icons): migrate from TSX components to raw SVG files
    - Replace all *.tsx icon components with raw SVG files
    - Refactor icon registry to create React components from SVG content
    - Automatically derive IconName type from registry keys
    - Update Icon component to use new registry architecture
    - Remove hardcoded component files in favor of centralized SVG storage

--- src/components/ui/Icon.tsx
+++ src/components/ui/Icon.tsx
@@ -5,9 +5,9 @@ import { iconRegistry, type IconName } from './icons';
 import { iconVariants } from '@/lib/theme/components/icon';
 
 export interface IconProps 
-  extends Omit<React.SVGProps<SVGSVGElement>, 'width' | 'height'>,
-    VariantProps<typeof iconVariants> {
+  extends Omit<React.SVGProps<SVGSVGElement>, 'width' | 'height'> {
   name: IconName;
+  size?: VariantProps<typeof iconVariants>['size'];
   className?: string;
 }
 
@@ -32,4 +32,3 @@ const Icon = React.forwardRef<SVGSVGElement, IconProps>(
 Icon.displayName = 'Icon';
 
 export { Icon, type IconName };
-
